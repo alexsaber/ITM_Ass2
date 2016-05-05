bcac9954eabe204da0c18a3b2518cdba5d7d6c99
Oleksandr_Shabelnyk <a1263258@unet.univie.ac.at>
2016-05-05 12:10:29 +0200

    2.6 beta, not working yet

--- java/itm/video/VideoThumbnailGenerator.java
+++ java/itm/video/VideoThumbnailGenerator.java
@@ -7,9 +7,22 @@ package itm.video;
 
 import itm.util.ImageCompare;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
+import com.xuggle.xuggler.Global;
+import com.xuggle.xuggler.IContainer;
+import com.xuggle.xuggler.IPacket;
+import com.xuggle.xuggler.IPixelFormat;
+import com.xuggle.xuggler.IRational;
+import com.xuggle.xuggler.IStreamCoder;
+import com.xuggle.xuggler.IVideoPicture;
+import com.xuggle.xuggler.IVideoResampler;
+import com.xuggle.xuggler.Utils;
 
 /**
  * This class reads video files, extracts metadata for both the audio and the
@@ -116,6 +129,131 @@ public class VideoThumbnailGenerator {
 
 		// extract frames from input video
 		
+		
+		//**********************************************
+		//source: https://gist.github.com/tuler/5713172
+		//***********************************************
+        IContainer container = IContainer.make(); 
+        container.open(input.getAbsolutePath(), IContainer.Type.READ, null);
+        //System.out.println("container.getDuration(): " + container.getDuration());
+        long durationInMs = container.getDuration();
+		if (durationInMs == Global.NO_PTS) {
+			throw new RuntimeException("Duration of container is unknown");
+		}
+        long durationInS = durationInMs/1000000;
+        
+        int numOfStreams = container.getNumStreams(); 
+        // and iterate through the streams to find the first video stream 
+        int theVidID = -1;
+        boolean vidIDFound = false;
+        IStreamCoder theVidCoder = null; 
+        for (int i = 0; i < numOfStreams; i++) { 
+                IStreamCoder coder = container.getStream(i).getStreamCoder(); 
+
+                if (coder.getCodecType() == com.xuggle.xuggler.ICodec.Type.CODEC_TYPE_VIDEO) { 
+                	theVidID = i; 
+                	theVidCoder = coder; 
+                	vidIDFound = true;
+                    break; 
+                } 
+        } 
+
+
+		if (vidIDFound == false)
+		   throw new RuntimeException( "No video stream in the file!");
+
+			
+        if (theVidCoder.open() < 0) 
+                throw new RuntimeException("Could not open video decoder!"); 
+
+        IVideoResampler resampler = null; 
+		// if this stream is not in BGR24, we're going to need to
+		// convert it.  The VideoResampler does that for us.
+        if (theVidCoder.getPixelType() != IPixelFormat.Type.BGR24) { 
+        	resampler = IVideoResampler.make(theVidCoder.getWidth(), 
+						            		 theVidCoder.getHeight(), 
+						            		 IPixelFormat.Type.BGR24, 
+						            		 theVidCoder.getWidth(), 
+						            		 theVidCoder.getHeight(), 
+						            		 theVidCoder.getPixelType()); 
+            if (resampler == null) 
+            	throw new RuntimeException("No color space!"); 
+        } 
+
+
+        
+        IRational timeBase = container.getStream(theVidID).getTimeBase(); 
+
+        //System.out.println("Timebase " + timeBase.toString()); 
+        
+        ArrayList<IVideoPicture> capturedFrames = new ArrayList<IVideoPicture>();
+        
+        for (int timeToCapture = 0; timeToCapture < durationInS; timeToCapture = timeToCapture + timespan){
+        	
+	        long time_stampToCapture = (timeBase.getDenominator() / timeBase.getNumerator()) * timeToCapture; 
+	        
+	       System.out.println("time_stampToCapture " + time_stampToCapture); 
+	        //long target = container.getStartTime() + timeStampOffset; 
+	
+	        container.seekKeyFrame(theVidID, time_stampToCapture, 0); 
+	        
+	        boolean isFinished = false; 
+	        
+	        IPacket packet = IPacket.make(); 
+	        while(container.readNextPacket(packet) >= 0 && !isFinished ) { 
+	        	System.out.println("inside while");
+	
+	                if (packet.getStreamIndex() == theVidID) { 
+	                	
+	            		IVideoPicture picture = IVideoPicture.make(theVidCoder.getPixelType(), 
+	                    											theVidCoder.getWidth(),
+	                    											theVidCoder.getHeight()); 
+	                    int offset = 0; 
+	                    while (offset < packet.getSize()) { 
+	                    	System.out.println("inside second while");
+	
+	                            int bytesDecoded = theVidCoder.decodeVideo(picture, packet, offset); 
+	                            if (bytesDecoded < 0) { 
+	                                    System.err.println("No video was decoded in the packet!"); 
+	                            } 
+	                            offset += bytesDecoded; 
+	
+	                            if (picture.isComplete()) { 
+	
+	                                    IVideoPicture newPic = picture; 
+	
+	                                    if (resampler != null) { 
+	                                        newPic = IVideoPicture.make(resampler.getOutputPixelFormat(), 
+	                                        							picture.getWidth(),
+	                                        							picture.getHeight()); 
+	                                        if (resampler.resample(newPic, picture) < 0) 
+	                                                throw new RuntimeException("Could not resample video!"); 
+	                                    } 
+	
+	                                    if (newPic.getPixelType() != IPixelFormat.Type.BGR24) 
+	                                            throw new RuntimeException("Could not decode video as BGR 24 bit data!"); 
+	
+	                                    BufferedImage screenshot = Utils.videoPictureToImage(newPic); 
+	
+	                                    File file = new File(outputFile.getAbsolutePath() + "-" + timeToCapture + ".jpg");
+	                                    
+	                                    ImageIO.write(screenshot, "jpg", file);
+	                                    isFinished = true; 
+	                            } 
+	                        } 
+	                } 
+	        } 
+	        
+        }
+
+    	theVidCoder.close(); 
+    	container.close(); 
+		
+		
+		
+		System.out.println("for " + input.getName() + " found " + capturedFrames.size() + " frames");
+		
+		
 		// add a watermark of your choice and paste it to the image
         // e.g. text or a graphic
 
@@ -138,6 +276,7 @@ public class VideoThumbnailGenerator {
 	 * information if required.
 	 */
 	public static void main(String[] args) throws Exception {
+		
 
 		if (args.length < 3) {
             System.out.println("usage: java itm.video.VideoThumbnailGenerator <input-video> <output-directory> <timespan>");
